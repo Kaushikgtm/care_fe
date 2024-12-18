c8a8d80fa8092130d43df7f818bff7149ebda2f8
Kaushikgtm <162317291+Kaushikgtm@users.noreply.github.com>
2024-12-18 20:59:20 +0530

    Update LoginPage.ts

--- cypress/pageobject/Login/LoginPage.ts
+++ cypress/pageobject/Login/LoginPage.ts
@@ -1,6 +1,17 @@
 import { users } from "../utils/userConfig";
 
+interface LanguageMapping {
+  [key: string]: {
+    login: string;
+    care: string;
+    goal: string;
+    footer_body: string;
+  };
+}
+
 class LoginPage {
+  languageSelector = "#language-selector";
+  submitButtonSelector: string = "#login-button"; 
   loginByRole(role: keyof typeof users): void {
     const user = users[role];
     if (!user) {
@@ -87,6 +98,55 @@ class LoginPage {
 
   verifyForgotPasswordHeading(text: string[]): void {
     cy.verifyContentPresence("#forgot-password-heading", text);
+  }
+    clickContributeOnGitHub(): void {
+    cy.get('a[href="https://github.com/ohcnetwork"]').scrollIntoView().click();
+  }
+
+  clickThirdPartyLicense(): void {
+    cy.get('a[href="/licenses"]').scrollIntoView().click();
+  }
+    selectLanguage(languageCode: string): void {
+    cy.get(this.languageSelector).select(languageCode);
+  }
+    switchLanguageAndVerifyButtonText(languageMappings: LanguageMapping): void {
+    Object.entries(languageMappings).forEach(([languageCode, texts]) => {
+      cy.get(this.languageSelector)
+        .find(`option[value="${languageCode}"]`)
+        .should("exist")
+        .then(($option) => {
+          if ($option.length === 0) {
+            throw new Error(`Language option ${languageCode} not found`);
+          }
+          this.selectLanguage(languageCode);
+          cy.get(this.submitButtonSelector, { timeout: 10000 })
+            .should("be.visible")
+            .should("have.text", texts.login)
+            .should("not.be.disabled");
+        });
+    });
+  }
+   private verifySidebarElement(selector: string, expectedText: string): void {
+    cy.get(selector, { timeout: 10000 })
+      .should("be.visible")
+      .should("have.text", expectedText)
+      .should("not.be.disabled");
+  }
+   switchLanguageAndVerifySidebars(languageMappings: LanguageMapping): void {
+    Object.entries(languageMappings).forEach(([languageCode, texts]) => {
+      cy.get(this.languageSelector)
+        .find(`option[value="${languageCode}"]`)
+        .should("exist")
+        .then(($option) => {
+          if ($option.length === 0) {
+            throw new Error(`Language option ${languageCode} not found`);
+          }
+          this.selectLanguage(languageCode);
+          this.verifySidebarElement("#care", texts.care);
+          this.verifySidebarElement("#goal", texts.goal);
+          this.verifySidebarElement("#footer_body", texts.footer_body);
+        });
+    });
   }
 }
 
